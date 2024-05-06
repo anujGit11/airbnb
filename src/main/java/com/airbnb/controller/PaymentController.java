382ea9ecdf3c7a5eb67621a687d91960110da9ab
anujGit11 <anuj777vishwakarma@gmail.com>
2024-05-06 11:18:00 +0530

    2nd commit

---
+++ src/main/java/com/airbnb/controller/PaymentController.java
@@ -0,0 +1,10 @@
+package com.airbnb.controller;
+
+public class PaymentController {
+
+    private String paymentType;
+
+    public PaymentController(String paymentType) {
+        this.paymentType = paymentType;
+    }
+}
